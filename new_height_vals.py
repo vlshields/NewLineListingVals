932fe62830394a9db7b3d1c89cd59a752227cdac
Vince Shields <vince.shields913@gmail.com>
2023-11-18 18:14:18 -0800

    modifyng listings

--- new_height_vals.py
+++ new_height_vals.py
@@ -19,43 +19,41 @@ def indicators(x):
     else:
         return "Both"
 
-def find_new(filename: str) -> None:
-    
-    height_old = pd.read_csv(filename)
-    
+def find_new(listing_1: str,listing_2: str,columns: list) -> None:
+
+    listing_1 = pd.read_csv(listing_1)
+    listing_2 = pd.read_csv(listing_2)
     # read most recent csv file from downloads folder and rename it. Make sure to
     # download the correct file first
-    
-    
-    qh = QuickHelper("heightvals")
-    height_new = qh.data
-    print(height_new.columns)
+
+
+
     try:
-        height_new = height_new[['Patient','CurrentHeight', "VisitDate","VisitNum"]]
-        height_old = height_old[['Patient','CurrentHeight', "VisitDate","VisitNum"]]
+        listing_2 = listing_2[columns]
+        listing_1 = listing_1[columns]
     except KeyError as e:
-        print("Did you remember to download the data grid?")
-        
-    
-    height_new["VisitDate"] = pd.to_datetime(height_new["VisitDate"])
-    height_new.sort_values(by=["Patient","VisitDate"],inplace=True)
-    
-    
-    height_old["VisitDate"] = pd.to_datetime(height_old["VisitDate"])
-    height_old.sort_values(by=["Patient","VisitDate"],inplace=True)
-    
-    
-    test = pd.merge(height_new,height_old,how='outer',indicator=True)
+        print("Are your column names matching")
+
+
+    # listing_2["VisitDate"] = pd.to_datetime(height_new["VisitDate"])
+    # listing_2.sort_values(by=["Patient","VisitDate"],inplace=True)
+    #
+    #
+    # listing_1["VisitDate"] = pd.to_datetime(height_old["VisitDate"])
+    # listing_1.sort_values(by=["Patient","VisitDate"],inplace=True)
+    #
+
+    test = pd.merge(listing_2,listing_1,how='outer',indicator=True)
     test.rename(columns={"_merge":"Indicator"},inplace=True)
-    
+
     test["Indicator"] = test["Indicator"].apply(indicators)
-    test["VisitDate"] = pd.to_datetime(test["VisitDate"])
-    test["VisitDate"] = test["VisitDate"].dt.strftime("%d-%b-%Y")
-    
+    # test["VisitDate"] = pd.to_datetime(test["VisitDate"])
+    # test["VisitDate"] = test["VisitDate"].dt.strftime("%d-%b-%Y")
+
     with pd.ExcelWriter("HeightNewVals{:%d_%b_%Y}.xlsx".format(date.today())) as writer:
-        
+
         workbook  = writer.book
-        
+
         test.to_excel(writer, sheet_name="Height",index=False)
         worksheet = writer.sheets["Height"]
         formatter = MyFormatter(test,workbook,worksheet)
@@ -67,13 +65,14 @@ def find_new(filename: str) -> None:
         worksheet.filter_column('E',"indicator == New or indicator == Old")
 
 def main():
-    parser = argparse.ArgumentParser(description="Find new height values.")
-    parser.add_argument("filename", type=str, help="Path to the csv file")
-    args = parser.parse_args()
-    find_new(args.filename)
+    listing_1 = input("Enter the old line listing: ")
+    listing_2 = input("Enter the new line listing: ")
+    columns = input("Enter the columns to match: ")
+    columns=columns.split(' ')
+    # parser = argparse.ArgumentParser(description="Find new height values.")
+    # parser.add_argument("filename", type=str, help="Path to the csv file")
+    # args = parser.parse_args()
+    # find_new(args.filename)
 
 if __name__ == "__main__":
     main()
-
-
-
